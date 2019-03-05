36d5762bdb7c353f7f1310489fe68e311881dc21
niccolotubini <tubini.niccolo@gmail.com>
2019-03-05 09:41:42 +0100

    JUnit test for the Readmsh class

---
+++ src/test/java/readtriangularization/TestReadmsh.java
@@ -0,0 +1,42 @@
+/*
+ * GNU GPL v3 License
+ *
+ * Copyright 2019 Niccolo` Tubini
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package readtriangularization;
+
+import org.junit.Test;
+
+public class TestReadmsh {
+
+	@Test
+	public void Test() throws Exception {
+
+		//String fileName = "resources/input/square22.msh";
+		String fileName = "resources/input/square_with_subdomain_100.msh";
+
+		String splitter = " ";
+
+		Readmsh reader = new Readmsh();
+		reader.fileName = fileName;
+		reader.splitter = splitter;
+		reader.checkData = false;
+		reader.process();
+		
+	}
+	
+}
